c60e7daadd0875c6e6553435e955f58d5dd25e43
trif0lium <gh@fleeingsunlight.dev>
2021-07-23 17:23:48 +0700

    build(react/tailwind): set `purge` option

--- react/tailwind.config.js
+++ react/tailwind.config.js
@@ -1,7 +1,11 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  purge: [],
+  purge: [
+    './src/**/*.html',
+    './src/**/*.vue',
+    './src/**/*.tsx',
+  ],
   presets: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
