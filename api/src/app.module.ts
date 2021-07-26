243e32cdfaa9e9fcc350c59ae617b188dc4f6f55
trif0lium <gh@fleeingsunlight.dev>
2021-07-26 15:49:28 +0700

    feat(app): init TypeOrmModule

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,12 +3,27 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HospitelModule } from './hospitel/hospitel.module';
 import { HospitelGateway } from './hospitel.gateway';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: process.env.NODE_ENV === 'development' && '.env.development',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('POSTGRES_HOST'),
+        port: +configService.get<number>('POSTGRES_PORT'),
+        username: configService.get('POSTGRES_USER'),
+        password: configService.get('POSTGRES_PASSWORD'),
+        database: configService.get('POSTGRES_DB'),
+        entities: [],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
     }),
     HospitelModule,
   ],
