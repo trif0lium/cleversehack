82fadb0af53d8f8b516bd35de46731696e88e37e
trif0lium <gh@fleeingsunlight.dev>
2021-07-27 12:57:14 +0700

    feat(hospitelcontroller): use GetHospitelDTO

--- api/src/hospitel/hospitel.controller.ts
+++ api/src/hospitel/hospitel.controller.ts
@@ -7,15 +7,15 @@ import {
   Post,
 } from '@nestjs/common';
 import { Hospitel } from 'src/datamodel/hospitel.datamodel';
-import { CreateHospitelDTO } from './hospitel.dto';
+import { CreateHospitelDTO, GetHospitelDTO } from './hospitel.dto';
 import { HospitelService } from './hospitel.service';
 
 @Controller('hospitel')
 export class HospitelController {
   constructor(private readonly hospitelService: HospitelService) {}
 
-  @Get()
-  list(): Promise<Hospitel[]> {
+  @Post()
+  list(@Body() data: GetHospitelDTO): Promise<Hospitel[]> {
     return this.hospitelService.findAll();
   }
 
