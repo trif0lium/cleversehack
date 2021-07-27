ec2dc8ee915d6c6e8a41a8a8af534cdffcb3b5ec
trif0lium <gh@fleeingsunlight.dev>
2021-07-27 17:06:21 +0700

    feat(application-config): init

---
+++ api/src/application-config.ts
@@ -0,0 +1,41 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+
+export interface ApplicationConfig {
+  database: TypeOrmModuleOptions;
+}
+
+export const applicationConfig = (): ApplicationConfig => {
+  if (process.env.NODE_ENV === 'development') {
+    return {
+      database: {
+        type: 'postgres',
+        host: process.env.POSTGRES_HOST,
+        port: +process.env.POSTGRES_PORT,
+        username: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD,
+        database: process.env.POSTGRES_DB,
+        synchronize: true,
+        namingStrategy: new SnakeNamingStrategy(),
+        extra: {
+          charset: 'utf8_unicode_ci',
+        },
+      },
+    };
+  }
+
+  return {
+    database: {
+      type: 'postgres',
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      synchronize: true,
+      namingStrategy: new SnakeNamingStrategy(),
+      extra: {
+        charset: 'utf8_unicode_ci',
+        socketPath: `/cloudsql/${process.env.DATABASE_CONNECTION_NAME}`,
+      },
+    },
+  };
+};
