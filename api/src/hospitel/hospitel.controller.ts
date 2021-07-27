d65b788ac3cad0b2c6a10ee110ccc2b8a1dfc827
trif0lium <gh@fleeingsunlight.dev>
2021-07-27 09:05:05 +0700

    feat(hospitel): add list() to HospitelController

--- api/src/hospitel/hospitel.controller.ts
+++ api/src/hospitel/hospitel.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { Hospitel } from 'src/datamodel/hospitel.datamodel';
+import { HospitelService } from './hospitel.service';
 
 @Controller('hospitel')
-export class HospitelController {}
+export class HospitelController {
+  constructor(private readonly hospitelService: HospitelService) {}
+
+  @Get()
+  list(): Promise<Hospitel[]> {
+    return this.hospitelService.findAll();
+  }
+}
