7f2ad7dfca18f31a1c2bf7d3c717f1273b927aa9
trif0lium <gh@fleeingsunlight.dev>
2021-07-27 09:03:04 +0700

    feat(hospitel): add findAll() + increaseCurrentCapacity()

--- api/src/hospitel/hospitel.service.ts
+++ api/src/hospitel/hospitel.service.ts
@@ -1,4 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Hospitel } from 'src/datamodel/hospitel.datamodel';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class HospitelService {}
+export class HospitelService {
+  constructor(
+    @InjectRepository(Hospitel)
+    private hospitelRepository: Repository<Hospitel>,
+  ) {}
+
+  findAll(): Promise<Hospitel[]> {
+    return this.hospitelRepository.find({});
+  }
+
+  async increaseCurrentCapacity(hospitelCode: string, n: number) {
+    const hospitel = await this.hospitelRepository.findOneOrFail({
+      code: hospitelCode,
+    });
+
+    let maxCapacity = hospitel.maxCapacity;
+    if (hospitel.currentCapacity + n > hospitel.maxCapacity) {
+      maxCapacity = hospitel.currentCapacity + n;
+    }
+
+    await this.hospitelRepository.update(
+      { id: hospitel.id },
+      { currentCapacity: hospitel.currentCapacity + n, maxCapacity },
+    );
+  }
+}
