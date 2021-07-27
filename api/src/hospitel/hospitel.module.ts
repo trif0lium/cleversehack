bf6b8116a5d22307f9cff354834b1c800f070fe7
trif0lium <gh@fleeingsunlight.dev>
2021-07-27 09:13:14 +0700

    fix(hospitel): add TypeOrmModule.forFeature()

--- api/src/hospitel/hospitel.module.ts
+++ api/src/hospitel/hospitel.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Hospitel } from 'src/datamodel/hospitel.datamodel';
 import { HospitelController } from './hospitel.controller';
 import { HospitelService } from './hospitel.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Hospitel])],
   controllers: [HospitelController],
   providers: [HospitelService],
   exports: [HospitelService],
