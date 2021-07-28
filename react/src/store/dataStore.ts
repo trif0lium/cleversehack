ed433859276662ddf5706dba7cb274df62c65304
trif0lium <gh@fleeingsunlight.dev>
2021-07-28 16:18:44 +0700

    feat(store/dataStore): add init() + setHospitelList()

--- react/src/store/dataStore.ts
+++ react/src/store/dataStore.ts
@@ -1,8 +1,26 @@
-import { makeAutoObservable } from "mobx";
+import { action, makeAutoObservable } from "mobx";
+import axios from "axios";
+
+const API_URL =
+  "https://cleversehack-api-dot-everyday-development.et.r.appspot.com";
 
 class DataStore {
+  hospitelList: Hospitel[] = [];
+
   constructor() {
     makeAutoObservable(this);
+  }
+
+  async init() {
+    try {
+      const { data } = await axios.get<Hospitel[]>(`${API_URL}/hospitel`);
+      this.setHospitelList(data);
+    } catch (_) {}
+  }
+
+  @action
+  setHospitelList(list: Hospitel[]) {
+    this.hospitelList = list;
   }
 }
 
