8d88ba8d72e89547045c5360746aa8633e3f7d7e
trif0lium <gh@fleeingsunlight.dev>
2021-07-28 13:53:46 +0700

    feat(hack/migrate): add `Input` interface

--- hack/migrate.ts
+++ hack/migrate.ts
@@ -2,6 +2,19 @@ import csv from "csv-parser";
 import fs from "fs";
 const results = [];
 
+interface Input {
+  name: string;
+  maxCapacity: number;
+  hospital: string;
+  location: string;
+  address: string;
+  phoneNumber?: string;
+  website?: string;
+  G: boolean;
+  Y: boolean;
+  R: boolean;
+}
+
 fs.createReadStream("data.csv")
   .pipe(csv())
   .on("data", (data) => results.push(data))
