8f14dc63d18f1ad1e9ed79e52dd7ee9885f76901
trif0lium <gh@fleeingsunlight.dev>
2021-07-29 19:13:41 +0700

    feat(store/searchStore): add initial implementation for SearchStore

--- react/src/store/searchStore.ts
+++ react/src/store/searchStore.ts
@@ -1,7 +1,33 @@
-import { makeAutoObservable } from "mobx";
+import { action, makeAutoObservable } from "mobx";
 
 export class SearchStore {
   constructor() {
     makeAutoObservable(this);
+  }
+
+  search: string = "";
+  filters: Array<"ONLY_HOSPITEL" | "ONLY_HOSPITAL" | "ONLY_AVAILABLE"> = [];
+  sort: { field: string; direction: "asc" | "desc" } | null = null;
+
+  @action
+  setSearch(s: string) {
+    this.search = s;
+  }
+
+  @action
+  setFilters(f: Array<"ONLY_HOSPITEL" | "ONLY_HOSPITAL" | "ONLY_AVAILABLE">) {
+    this.filters = f;
+  }
+
+  @action
+  setSort(s: { field: string; direction: "asc" | "desc" } | null) {
+    this.sort = s;
+  }
+
+  @action
+  reset() {
+    this.setSearch("");
+    this.setFilters([]);
+    this.setSort(null);
   }
 }
