1fd0abfb55f4558d467e5c07bc74388abd2f8088
trif0lium <gh@fleeingsunlight.dev>
2021-07-29 22:44:05 +0700

    feat(SearchLocationDetailList): add relativeDistance

--- react/src/components/search-location/SearchLocationDetailList.tsx
+++ react/src/components/search-location/SearchLocationDetailList.tsx
@@ -1,29 +1,21 @@
-import React, { useEffect } from "react";
-import { DetailList } from "../styles/DetailListStyles";
-import { MOCK_DATA } from "../const";
-import { SearchLocationCard } from "./SearchLocationCard";
-import { SearchBarSelectOption } from "./search-location";
-import { observer } from "mobx-react-lite";
-import { useHospitelList } from "../../hooks/useHospitelList";
+import React from 'react';
+import { DetailList } from '../styles/DetailListStyles';
+import { SearchLocationCard } from './SearchLocationCard';
+import { observer } from 'mobx-react-lite';
+import { useHospitelList } from '../../hooks/useHospitelList';
 
-interface SearchLocationContentProps {
-  searchTerm: string;
-  sortBy: SearchBarSelectOption;
-  options: [];
-}
-
-const _SearchLocationDetailList = ({
-  searchTerm,
-  sortBy,
-  options,
-}: SearchLocationContentProps) => {
-  const { hospitelList } = useHospitelList();
+const _SearchLocationDetailList = () => {
+  const { hospitelList, relativeDistanceList } = useHospitelList();
 
   return (
     <DetailList>
       <div className="location-detail-list pt-5 md:grid md:grid-cols-2 lg:grid-cols-3 px-5">
         {hospitelList.map((location) => (
-          <SearchLocationCard key={location.code} selectedLocation={location} />
+          <SearchLocationCard
+            key={location.code}
+            selectedLocation={location}
+            relativeDistance={relativeDistanceList[location.code]}
+          />
         ))}
       </div>
     </DetailList>
