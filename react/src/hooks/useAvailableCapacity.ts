0e1e4f05ee8c9eb59e6f4f16610b47c78a49be9c
trif0lium <gh@fleeingsunlight.dev>
2021-07-30 12:57:35 +0700

    feat(hooks/useAvailableCapacity): calculate percent

--- react/src/hooks/useAvailableCapacity.ts
+++ react/src/hooks/useAvailableCapacity.ts
@@ -1,1 +1,18 @@
-export function useAvailableCapacity() {}
+import { useMemo } from 'react';
+
+export function useAvailableCapacity(
+  currentCapacity: number,
+  maxCapacity: number,
+) {
+  const availableCapacity = useMemo(
+    () => maxCapacity - currentCapacity,
+    [currentCapacity, maxCapacity],
+  );
+
+  const availableCapacityPercent = useMemo(
+    () => (availableCapacity * 100) / maxCapacity,
+    [availableCapacity],
+  );
+
+  return { availableCapacity, availableCapacityPercent };
+}
