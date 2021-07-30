c2126b449ac01b9c78a108109b0362fbd793369e
trif0lium <gh@fleeingsunlight.dev>
2021-07-30 08:31:57 +0700

    feat(hack/simulate): perform PATCH request

--- hack/simulate-websocket.ts
+++ hack/simulate-websocket.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
-import { sampleSize } from "lodash";
+import { random, sampleSize } from "lodash";
+import pLimit from "p-limit";
 
 const API_URL =
   "https://cleversehack-api-dot-everyday-development.et.r.appspot.com";
@@ -20,4 +21,17 @@ function randomInt(inclusiveMin: number, exclusiveMax: number): number {
   >(`${API_URL}/hospitels`);
 
   const samples = sampleSize(hospitels, randomInt(1, 6));
+
+  const limit = pLimit(5);
+  const requests = samples.map((s) =>
+    limit(async () => {
+      try {
+        await axios.patch(`${API_URL}/hospitels/${s.code}`, {
+          direction: ["INC", "DEC"][random(0, 1, false)],
+          n: 1,
+        });
+      } catch (_) {}
+    })
+  );
+  await Promise.all(requests);
 })();
