50b46a4e0e0aeb3f792889ed9c040e07c5f1c961
trif0lium <gh@fleeingsunlight.dev>
2021-07-30 08:19:29 +0700

    fix(hack/simulate): return type is array

--- hack/simulate-websocket.ts
+++ hack/simulate-websocket.ts
@@ -4,9 +4,11 @@ const API_URL =
   "https://cleversehack-api-dot-everyday-development.et.r.appspot.com";
 
 (async () => {
-  const { data: hospitels } = await axios.get<{
-    code: string;
-    maxCapacity: number;
-    currentCapacity: number;
-  }>(`${API_URL}/hospitels`);
+  const { data: hospitels } = await axios.get<
+    {
+      code: string;
+      maxCapacity: number;
+      currentCapacity: number;
+    }[]
+  >(`${API_URL}/hospitels`);
 })();
