ba2f6a6f805224abf024c9fc89c596c6cfe47a96
trif0lium <gh@fleeingsunlight.dev>
2021-07-30 11:04:23 +0700

    feat(hooks/useRelativeTime): add useEffect()

--- react/src/hooks/useRelativeTime.ts
+++ react/src/hooks/useRelativeTime.ts
@@ -1,11 +1,17 @@
 import { DateTime } from 'luxon';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useInterval } from './useInterval';
 
 export function useRelativeTime(timestamp: number) {
   const [relativeTime, setRelativeTime] = useState<string>(
     DateTime.fromMillis(timestamp).toRelative({ locale: 'th-TH' }) ?? '',
   );
+
+  useEffect(() => {
+    setRelativeTime(
+      DateTime.fromMillis(timestamp).toRelative({ locale: 'th-TH' }) ?? '',
+    );
+  }, []);
 
   useInterval(() => {
     setRelativeTime(
