16e6f74efe3c53189707e072dffcb73908273f4a
trif0lium <gh@fleeingsunlight.dev>
2021-07-30 08:34:02 +0700

    chore(hack/simulate): random(5, hospitels.length)

--- hack/simulate-websocket.ts
+++ hack/simulate-websocket.ts
@@ -5,12 +5,6 @@ import pLimit from "p-limit";
 const API_URL =
   "https://cleversehack-api-dot-everyday-development.et.r.appspot.com";
 
-function randomInt(inclusiveMin: number, exclusiveMax: number): number {
-  const min = Math.ceil(inclusiveMin);
-  const max = Math.floor(exclusiveMax);
-  return Math.floor(Math.random() * (max - min) + min);
-}
-
 (async () => {
   const { data: hospitels } = await axios.get<
     {
@@ -20,7 +14,7 @@ function randomInt(inclusiveMin: number, exclusiveMax: number): number {
     }[]
   >(`${API_URL}/hospitels`);
 
-  const samples = sampleSize(hospitels, randomInt(1, 6));
+  const samples = sampleSize(hospitels, random(5, hospitels.length));
 
   const limit = pLimit(5);
   const requests = samples.map((s) =>
