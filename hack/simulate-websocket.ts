233bc25fd0208a6107ba050145a94c427f82d87e
trif0lium <gh@fleeingsunlight.dev>
2021-07-30 08:26:47 +0700

    feat(hack/simulate): get samples

--- hack/simulate-websocket.ts
+++ hack/simulate-websocket.ts
@@ -1,7 +1,14 @@
 import axios from "axios";
+import { sampleSize } from "lodash";
 
 const API_URL =
   "https://cleversehack-api-dot-everyday-development.et.r.appspot.com";
+
+function randomInt(inclusiveMin: number, exclusiveMax: number): number {
+  const min = Math.ceil(inclusiveMin);
+  const max = Math.floor(exclusiveMax);
+  return Math.floor(Math.random() * (max - min) + min);
+}
 
 (async () => {
   const { data: hospitels } = await axios.get<
@@ -11,4 +18,6 @@ const API_URL =
       currentCapacity: number;
     }[]
   >(`${API_URL}/hospitels`);
+
+  const samples = sampleSize(hospitels, randomInt(1, 6));
 })();
