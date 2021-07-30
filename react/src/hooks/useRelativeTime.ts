027b359156c1c43c23c470d9e7d3d539a35f746c
trif0lium <gh@fleeingsunlight.dev>
2021-07-30 10:06:24 +0700

    feat(hooks/useRelativeTime): assign initial value to relativeTime

--- react/src/hooks/useRelativeTime.ts
+++ react/src/hooks/useRelativeTime.ts
@@ -3,11 +3,15 @@ import { useState } from 'react';
 import { useInterval } from './useInterval';
 
 export function useRelativeTime(timestamp: number) {
-  const [relativeTime, setRelativeTime] = useState<string>('');
+  const [relativeTime, setRelativeTime] = useState<string>(
+    DateTime.fromMillis(timestamp).toRelative({ locale: 'th-TH' }) ?? '',
+  );
+
   useInterval(() => {
     setRelativeTime(
       DateTime.fromMillis(timestamp).toRelative({ locale: 'th-TH' }) ?? '',
     );
   }, 5000);
+
   return { relativeTime };
 }
