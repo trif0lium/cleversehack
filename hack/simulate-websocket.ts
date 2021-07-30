52d339b992c425449f8d28c25d6a086b3d6252c6
trif0lium <gh@fleeingsunlight.dev>
2021-07-30 08:16:33 +0700

    feat(hack/simulate): get initial list

--- hack/simulate-websocket.ts
+++ hack/simulate-websocket.ts
@@ -2,3 +2,11 @@ import axios from "axios";
 
 const API_URL =
   "https://cleversehack-api-dot-everyday-development.et.r.appspot.com";
+
+(async () => {
+  const { data: hospitels } = await axios.get<{
+    code: string;
+    maxCapacity: number;
+    currentCapacity: number;
+  }>(`${API_URL}/hospitels`);
+})();
